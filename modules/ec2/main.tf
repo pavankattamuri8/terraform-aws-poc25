689d8735a6c27cca251d3fd710d8ca5907a80a2f
pavankattamuri8 <147482466+pavankattamuri8@users.noreply.github.com>
2026-05-12 23:24:47 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+  subnet_id     = var.subnet_id
+  security_groups = [aws_security_group.sg.id]
+}
+
+output "instance_id" {
+  value = aws_instance.this.id
+}
